5ff7e5906cc63222952a1922ae995b847fb47420
vs-darsh-viroja <darsh.vsquare.team@gmail.com>
2025-09-24 14:43:40 +0530

    Initial Commit

---
+++ NeoLed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NeoLed
+//
+//  Created by Purvi Sancheti on 24/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NeoLed/NeoLedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeoLedApp.swift
+//  NeoLed
+//
+//  Created by Purvi Sancheti on 24/09/25.
+//
+
+import SwiftUI
+
+@main
+struct NeoLedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
